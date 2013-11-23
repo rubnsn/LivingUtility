329163bd6b08e73e9e02816f0b11b2439c81cae8
Schr0 <sch0dinger7@gmail.com>
2013-11-23 21:22:49 +0900

    (1)メイドさん的なアレを目指すための布石

---
+++ Schr0/LivingUtility/mods/api/ModeRegistry.java
@@ -0,0 +1,14 @@
+package Schr0.LivingUtility.mods.api;
+
+import Schr0.LivingUtility.mods.entity.EntityLivingUtility;
+
+public class ModeRegistry
+{
+	private EntityLivingUtility	owner;
+	
+	public ModeRegistry(EntityLivingUtility thheUtility)
+	{
+		this.owner = thheUtility;
+	}
+	
+}
